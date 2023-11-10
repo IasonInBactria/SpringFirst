22ea55e90322630755083dda694b969fffdbfe2e
IasonInBactria <swjasonyang@163.com>
2023-11-10 15:53:24 +0800

    IoC和DI入门

--- src/main/java/com/argo/service/impl/BookServiceImpl.java
+++ src/main/java/com/argo/service/impl/BookServiceImpl.java
@@ -6,7 +6,7 @@ import com.argo.service.BookService;
 
 public class BookServiceImpl implements BookService {
     //5.删除业务层中使用new的方式创建的dao对象
-    private BookDao bookDao = new BookDaoImpl();
+    private BookDao bookDao/* = new BookDaoImpl()*/;
 
     public void save() {
         System.out.println("book service save ...");
