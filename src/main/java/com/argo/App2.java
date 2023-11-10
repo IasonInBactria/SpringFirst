3c5c302ca12dd5e974563db789cf5e981a721a04
IasonInBactria <swjasonyang@163.com>
2023-11-10 15:37:05 +0800

    IoC入门

--- src/main/java/com/argo/App2.java
+++ src/main/java/com/argo/App2.java
@@ -1,6 +1,7 @@
 package com.argo;
 
 import com.argo.dao.BookDao;
+import com.argo.service.BookService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,6 +10,8 @@ public class App2 {
         //获取IoC设置
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         //获取bean
-        BookDao bookDao = (BookDao) context.getBean("bookDao");
+//        BookDao bookDao = (BookDao) context.getBean("bookDao");
+        BookService service = (BookService) context.getBean("bookService");
+        service.save();
     }
 }
--- src/main/java/com/argo/service/impl/BookServiceImpl.java
+++ src/main/java/com/argo/service/impl/BookServiceImpl.java
@@ -1,11 +1,12 @@
 package com.argo.service.impl;
 
 import com.argo.dao.BookDao;
+import com.argo.dao.impl.BookDaoImpl;
 import com.argo.service.BookService;
 
 public class BookServiceImpl implements BookService {
     //5.删除业务层中使用new的方式创建的dao对象
-    private BookDao bookDao;
+    private BookDao bookDao = new BookDaoImpl();
 
     public void save() {
         System.out.println("book service save ...");
